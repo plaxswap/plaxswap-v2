5b4d9714bac6aab16a91ca8f26bf51e143c95c6f
plaxswap <plaxswap@gmail.com>
2025-05-24 01:29:48 +0800

    update

--- apps/web/src/components/ConnectWalletButton.tsx
+++ apps/web/src/components/ConnectWalletButton.tsx
@@ -1,61 +1,48 @@
 import { useTranslation } from '@pancakeswap/localization'
 import { Button, ButtonProps } from '@pancakeswap/uikit'
-import { useActiveChainId } from 'hooks/useActiveChainId'
 import useAuth from 'hooks/useAuth'
 // @ts-ignore
 // eslint-disable-next-line import/extensions
 import { useActiveHandle } from 'hooks/useEagerConnect.bmp.ts'
-import { useAccount, useConnect } from 'wagmi'
-import { useEffect, useRef } from 'react'
-import { useWeb3Modal } from '@web3modal/ethers5/react'
+import { useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers5/react'
+import { PropsWithChildren } from 'react'
 import { ConnectorNames } from 'config/wallet'
-import Trans from './Trans'
 
-
-const ConnectWalletButton = ({ children, ...props }: ButtonProps) => {
+const ConnectWalletButton = ({ children, ...props }: PropsWithChildren<ButtonProps>) => {
   const handleActive = useActiveHandle()
   const { login } = useAuth()
   const {
     t,
     currentLanguage: { code },
   } = useTranslation()
-
   const { open } = useWeb3Modal()
-  const { connector, isConnected } = useAccount()
-  const hasLoggedIn = useRef(false)
 
-  const handleClick = async () => {
-    if (typeof __NEZHA_BRIDGE__ !== 'undefined') {
-      handleActive()
-    } else {
-      hasLoggedIn.current = false
-      await open()
+
+  const handleReownLogin = async () => {
+    await open()
+    const { isConnected } = await __NEZHA_BRIDGE__.getAccount()
+    if (isConnected) {
+      login(ConnectorNames.Injected)
     }
   }
 
-  useEffect(() => {
-    const loginIfConnected = async () => {
-      if (isConnected && connector && !hasLoggedIn.current) {
-        hasLoggedIn.current = true
-        try {
-          const connectorId = connector.id as ConnectorNames
-          const result = await login(connectorId)
-          if (!result) {
-            console.error('Login failed')
-          }
-        } catch (err) {
-          console.error('Login error', err)
-        }
+  const handleClick = () => {
+    if (typeof __NEZHA_BRIDGE__ !== 'undefined') {
+      handleActive()
+    } else {
+      try {
+        handleReownLogin()
+      } finally {
+        // do nothing
       }
     }
+  }
 
-    loginIfConnected()
-  }, [isConnected, connector, login])
 
   return (
-    <Button onClick={handleClick} {...props}>
-      {children || <Trans>Connect Wallet</Trans>}
-    </Button>
+      <Button onClick={handleClick} variant="primary">
+        {children || t('Connect Wallet')}
+      </Button>
   )
 }
 
