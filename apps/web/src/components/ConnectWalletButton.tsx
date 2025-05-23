a9e88b392b1e1a9872a7ad419f9bf85dc8093fbf
plaxswap <plaxswap@gmail.com>
2025-05-23 22:12:55 +0800

    update

--- apps/web/src/components/ConnectWalletButton.tsx
+++ apps/web/src/components/ConnectWalletButton.tsx
@@ -1,47 +1,48 @@
-import { PropsWithChildren, useEffect, useState } from 'react'
+import { useTranslation } from '@pancakeswap/localization'
 import { Button, ButtonProps } from '@pancakeswap/uikit'
-import { useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers5/react'
 import useAuth from 'hooks/useAuth'
-import { ConnectorNames } from 'config/wallet'
 // @ts-ignore
 // eslint-disable-next-line import/extensions
 import { useActiveHandle } from 'hooks/useEagerConnect.bmp.ts'
+import { PropsWithChildren } from 'react'
+import { useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers5/react'
+import { ConnectorNames } from 'config/wallet'
 
 const ConnectWalletButton = ({ children, ...props }: PropsWithChildren<ButtonProps>) => {
+  const handleActive = useActiveHandle()
+  const { login } = useAuth()
   const { open: connectReown } = useWeb3Modal()
   const { isConnected } = useWeb3ModalAccount()
-  const { login } = useAuth()
-  const handleActive = useActiveHandle()
 
-  const [isConnecting, setIsConnecting] = useState(false)
-  const [hasLoggedIn, setHasLoggedIn] = useState(false)
+
+  
 
   const handleClick = async () => {
     if (typeof __NEZHA_BRIDGE__ !== 'undefined') {
       handleActive()
     } else {
-      setIsConnecting(true)
       try {
-        await connectReown()
-      } catch (err) {
-        console.error(err)
+        await connectReown().then(() => {
+  setTimeout(() => {
+    if (isConnected) {
+      login(ConnectorNames.Injected)
+    }
+  }, 300)
+})
       } finally {
-        setIsConnecting(false)
+        // do nothing
       }
     }
   }
 
-  useEffect(() => {
-    if (isConnected && !hasLoggedIn) {
-      login(ConnectorNames.Injected)
-      setHasLoggedIn(true)
-    }
-  }, [isConnected, hasLoggedIn])
 
   return (
-    <Button onClick={handleClick} disabled={isConnecting} {...props}>
-      {isConnecting ? 'Connecting...' : children || 'Connect Wallet'}
-    </Button>
+    <>
+      <Button onClick={handleClick} variant="primary">
+        {children || ('Connect Wallet')}
+      </Button>
+      
+    </>
   )
 }
 
