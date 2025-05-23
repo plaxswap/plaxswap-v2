e6838bd210d9f8cab1633d58ea5e6cbdf526bc89
plaxswap <plaxswap@gmail.com>
2025-05-23 19:29:22 +0800

    update

--- apps/web/src/components/ConnectWalletButton.tsx
+++ apps/web/src/components/ConnectWalletButton.tsx
@@ -1,47 +1,46 @@
-import { useTranslation } from '@pancakeswap/localization'
-import { Button, ButtonProps } from '@pancakeswap/uikit'
-import useAuth from 'hooks/useAuth'
-// @ts-ignore
-// eslint-disable-next-line import/extensions
-import { useActiveHandle } from 'hooks/useEagerConnect.bmp.ts'
-import { PropsWithChildren } from 'react'
+import { useEffect, useState } from 'react'
 import { useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers5/react'
+import { Button } from '@pancakeswap/uikit'
+import useAuth from 'hooks/useAuth'
+import { useActiveHandle } from 'hooks/useEagerConnect.bmp.ts'
 import { ConnectorNames } from 'config/wallet'
 
-const ConnectWalletButton = ({ children, ...props }: PropsWithChildren<ButtonProps>) => {
-  const handleActive = useActiveHandle()
-  const { login } = useAuth()
+const ConnectWalletButton = () => {
   const { open: connectReown } = useWeb3Modal()
   const { isConnected } = useWeb3ModalAccount()
+  const { login } = useAuth()
+  const handleActive = useActiveHandle()
 
-
-  const handleReownLogin = async () => {
-    if (isConnected) {
-      login(ConnectorNames.Injected)
-    }
-  }
+  const [isConnecting, setIsConnecting] = useState(false)
+  const [hasLoggedIn, setHasLoggedIn] = useState(false)
 
   const handleClick = async () => {
     if (typeof __NEZHA_BRIDGE__ !== 'undefined') {
       handleActive()
     } else {
+      setIsConnecting(true)
       try {
         await connectReown()
-        await handleReownLogin()
+      } catch (err) {
+        console.error(err)
       } finally {
-        // do nothing
+        setIsConnecting(false)
       }
     }
   }
 
+  // login otomatis saat isConnected berubah jadi true
+  useEffect(() => {
+    if (isConnected && !hasLoggedIn) {
+      login(ConnectorNames.Injected)
+      setHasLoggedIn(true)
+    }
+  }, [isConnected, hasLoggedIn])
 
   return (
-    <>
-      <Button onClick={handleClick} variant="primary">
-        {children || ('Connect Wallet')}
-      </Button>
-      
-    </>
+    <Button onClick={handleClick} disabled={isConnecting}>
+      {isConnecting ? 'Connecting...' : 'Connect Wallet'}
+    </Button>
   )
 }
 
