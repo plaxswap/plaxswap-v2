4068ee417ad5f0b1265672fc49f853337c5691f4
plaxswap <plaxswap@gmail.com>
2025-05-23 19:50:59 +0800

    update

--- apps/web/src/components/ConnectWalletButton.tsx
+++ apps/web/src/components/ConnectWalletButton.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react'
+import { PropsWithChildren, useEffect, useState } from 'react'
+import { Button, ButtonProps } from '@pancakeswap/uikit'
 import { useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers5/react'
-import { Button } from '@pancakeswap/uikit'
 import useAuth from 'hooks/useAuth'
+import { ConnectorNames } from 'config/wallet'
 // @ts-ignore
 // eslint-disable-next-line import/extensions
 import { useActiveHandle } from 'hooks/useEagerConnect.bmp.ts'
-import { ConnectorNames } from 'config/wallet'
 
-const ConnectWalletButton = () => {
+const ConnectWalletButton = ({ children, ...props }: PropsWithChildren<ButtonProps>) => {
   const { open: connectReown } = useWeb3Modal()
   const { isConnected } = useWeb3ModalAccount()
   const { login } = useAuth()
@@ -31,7 +31,6 @@ const ConnectWalletButton = () => {
     }
   }
 
-  // login otomatis saat isConnected berubah jadi true
   useEffect(() => {
     if (isConnected && !hasLoggedIn) {
       login(ConnectorNames.Injected)
@@ -40,8 +39,8 @@ const ConnectWalletButton = () => {
   }, [isConnected, hasLoggedIn])
 
   return (
-    <Button onClick={handleClick} disabled={isConnecting}>
-      {isConnecting ? 'Connecting...' : 'Connect Wallet'}
+    <Button onClick={handleClick} disabled={isConnecting} {...props}>
+      {isConnecting ? 'Connecting...' : children || 'Connect Wallet'}
     </Button>
   )
 }
